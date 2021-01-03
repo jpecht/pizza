339c3f895fe34a3ced0fc7b73779790c80ff17fe
jpecht <me.jefferson@gmail.com>
2021-01-03 12:52:52 -0700

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAE5C_pHxEmRSlIloQLgPtPb2CF_1RZBWE",
+  authDomain: "pizza-58c02.firebaseapp.com",
+  databaseURL: "https://pizza-58c02-default-rtdb.firebaseio.com",
+  projectId: "pizza-58c02",
+  storageBucket: "pizza-58c02.appspot.com",
+  messagingSenderId: "367689442981",
+  appId: "1:367689442981:web:61774f2578c825a71b8f4c",
+  measurementId: "G-BEF04ZEKTL"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <h1 class="header">
+      pizza in denver
+    </h1>
+    <h2 class="subheader">
+      reviewed by the most trusted
+    </h2>
+    <div class="listContainer">
+
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import firebase from 'firebase';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+
+  data: () => ({
+    locations: [],
+  }),
+
+  created() {
+    const db = firebase.firestore();
+    db.collection('locations').doc('Test').set({
+      name: 'Test Location',
+      location: 'Denver',
+    });    
   },
 });
 </script>
+
+<style lang="scss">
+.container {
+  padding: 10px 25px;
+  text-align: left;
+}
+
+.header {
+  font-size: 36px;
+}
+
+.subheader {
+  font-size: 18px;
+  font-weight: 300;
+}
+</style>
